5c934a22d2c955f25594013279b794e8d4b85168
pho9902 <bagh9902@gmail.com>
2026-03-20 19:22:47 +0900

    stage 1 코드 작성

--- src/stages/stage1/Stage1.jsx
+++ src/stages/stage1/Stage1.jsx
@@ -1,34 +1,60 @@
-import styled from 'styled-components';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
-
-// TODO: todo.json 데이터를 import 하세요
-// TODO: useState를 import 하세요
+import styled from "styled-components";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import * as todoData from "../../data/todo.json";
+import { useState } from "react";
 
 export default function Stage1() {
-  // TODO: todos 상태를 선언하세요 (초기값: todo.json에서 불러온 데이터)
+  const [datas, setDatas] = useState(todoData.default);
 
-  // TODO: 할 일 추가 함수를 만드세요
-  // HINT: 새 todo 객체에는 id, title, completed, priority, createdAt 필드가 필요합니다
+  function onAdd(title) {
+    setDatas([
+      ...datas,
+      {
+        id: datas.length && datas[datas.length - 1].id + 1,
+        title,
+        completed: false,
+        priority: "medium",
+        createdAt: new Date().toISOString(),
+      },
+    ]);
+  }
 
-  // TODO: 할 일 완료/미완료 토글 함수를 만드세요
+  function onToggle(id) {
+    setDatas(
+      datas.map((data) =>
+        data.id === id ? { ...data, completed: !data.completed } : data,
+      ),
+    );
+  }
 
-  // TODO: 할 일 삭제 함수를 만드세요
+  function onDelete(id) {
+    setDatas(datas.filter((data) => data.id !== id));
+  }
 
-  // TODO: 할 일 이름 변경 함수를 만드세요
+  function onRename(id, newTitle) {
+    setDatas(
+      datas.map((data) =>
+        data.id === id ? { ...data, title: newTitle } : data,
+      ),
+    );
+  }
 
   return (
     <Container>
       <Description>
         기본 CRUD — 할 일 추가, 삭제, 완료 토글, 이름 변경
       </Description>
-      {/* TODO: TodoForm과 TodoList 컴포넌트에 필요한 props를 전달하세요 */}
-      <TodoForm />
-      <TodoList />
+      <TodoForm onAdd={onAdd} />
+      <TodoList
+        todos={datas}
+        onToggle={onToggle}
+        onDelete={onDelete}
+        onRename={onRename}
+      />
     </Container>
   );
 }
-
 // ===== 아래는 스타일 코드입니다. 수정하지 않아도 됩니다. =====
 const Container = styled.section``;
 
