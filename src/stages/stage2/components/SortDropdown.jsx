b732f8f9240c326e6e6014b2741ea9a21001e3d1
Hanpla <hanpla25@gmail.com>
2026-03-24 15:16:57 +0900

    feat: stage2 완료

--- src/stages/stage2/components/SortDropdown.jsx
+++ src/stages/stage2/components/SortDropdown.jsx
@@ -1,15 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // HINT: 정렬 옵션 배열을 만들어 활용하면 편리합니다
 // 예) [{ value: 'createdAt', label: '생성일자순' }, ...]
+const SORTBY_OPTION = [
+  { value: "createdAt", label: "생성일자순" },
+  { value: "priority", label: "우선순위순" },
+  { value: "title", label: "가나다순" },
+];
 
 export default function SortDropdown({ sortBy, onSortChange }) {
   // TODO: select 요소의 value와 onChange를 연결하세요
   // TODO: 정렬 옵션들을 option 태그로 렌더링하세요
 
   return (
-    <Select>
-      {/* 여기에 option 태그들을 렌더링하세요 */}
+    <Select value={sortBy} onChange={onSortChange}>
+      {SORTBY_OPTION.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
     </Select>
   );
 }
--- src/stages/stage2/components/TodoItem.jsx
+++ src/stages/stage2/components/TodoItem.jsx
@@ -1,26 +1,74 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // TODO: useState를 import 하세요
+import { useState } from "react";
+
+const PRIORITY_LABELS = {
+  high: "높음",
+  medium: "보통",
+  low: "낮음",
+};
 
 export default function TodoItem({ todo, onToggle, onDelete, onRename }) {
   // TODO: 수정 모드 상태(isEditing)와 수정 텍스트(editValue) state를 선언하세요
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(todo.title);
 
   // TODO: 더블클릭 시 수정 모드로 전환하는 함수를 만드세요
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
 
   // TODO: 키보드 이벤트 핸들러를 만드세요 (Enter: 확정, Escape: 취소)
+  const handleEditKeydown = (e) => {
+    if (e.key === "Enter") {
+      onRename(todo.id, editValue);
+      setIsEditing(false);
+    }
+
+    if (e.key === "Escape") {
+      setEditValue(todo.title);
+      setIsEditing(false);
+    }
+  };
 
   // TODO: blur 이벤트 핸들러를 만드세요
+  const handleEditBlur = () => {
+    onRename(todo.id, editValue);
+    setIsEditing(false);
+  };
 
   return (
     <Container>
       {/* TODO: 체크박스를 렌더링하세요 */}
+      <Checkbox
+        type="checkbox"
+        onChange={() => onToggle(todo.id)}
+        checked={todo.completed}
+      />
 
       {/* TODO: 우선순위 배지를 렌더링하세요 */}
       {/* HINT: PriorityBadge 컴포넌트에 $priority prop을 전달하세요 */}
+      <PriorityBadge $priority={todo.priority}>
+        {PRIORITY_LABELS[todo.priority]}
+      </PriorityBadge>
 
       {/* TODO: 수정 모드에 따라 EditInput 또는 Title을 렌더링하세요 */}
+      {isEditing ? (
+        <EditInput
+          value={editValue}
+          onChange={(e) => setEditValue(e.target.value)}
+          onKeyDown={handleEditKeydown}
+          onBlur={handleEditBlur}
+        />
+      ) : (
+        <Title onDoubleClick={handleDoubleClick} $completed={todo.completed}>
+          {todo.title}
+        </Title>
+      )}
 
       {/* TODO: 삭제 버튼을 렌더링하세요 */}
+      <DeleteButton onClick={() => onDelete(todo.id)}>삭제</DeleteButton>
     </Container>
   );
 }
@@ -65,7 +113,7 @@ const Title = styled.span`
   color: ${({ $completed, theme }) =>
     $completed ? theme.text.completed : theme.text.primary};
   text-decoration: ${({ $completed }) =>
-    $completed ? 'line-through' : 'none'};
+    $completed ? "line-through" : "none"};
   cursor: default;
   user-select: none;
 `;
